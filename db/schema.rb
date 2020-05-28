a544e32203cb4039b4a8a38aaa54e075515f36ac
shin-nkt-09 <shin.nkt.09@gmail.com>
2020-05-28 19:49:01 +0900

    mypage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_084634) do
+ActiveRecord::Schema.define(version: 2020_05_27_113506) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_05_26_084634) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "reply_comment"
+  end
+
+  create_table "inquiries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
